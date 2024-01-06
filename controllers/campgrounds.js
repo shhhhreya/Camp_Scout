363a825bd14ca89f303d7ed0c0f8a7734e07d4ca
Shreya <shreya.singh.cseaiml.2020@miet.ac.in>
2024-01-06 16:31:29 +0530

    yelp camp

--- controllers/campgrounds.js
+++ controllers/campgrounds.js
@@ -18,7 +18,6 @@ module.exports.createCampground = async (req, res, next) => {
         query: req.body.campground.location,
         limit: 1
     }).send()
-    //res.send("OKKKKKKKKK")
     const campground = new Campground(req.body.campground);
     campground.geometry = geoData.body.features[0].geometry;
     campground.images = req.files.map(f=>({url: f.path, filename:f.filename}));
--- seeds/index.js
+++ seeds/index.js
@@ -25,7 +25,7 @@ const seedDB = async () => {
             author: '657978f8a54efb70687de9e2',
             location: `${cities[random1000].city}, ${cities[random1000].state}`,
             title: `${sample(descriptors)} ${sample(places)}`,
-            description:'Lorem Ipsum is simply dummy text of the printing and typesetting industry. Lorem Ipsum has been the industrys standard dummy text ever since the 1500s, when an unknown printer took a galley of type and scrambled it to make a type specimen book. It has survived not only five centuries, but also the leap into electronic typesetting, remaining essentially unchanged. It was popularised in the 1960s with the release of Letraset sheets containing Lorem Ipsum passages, and more recently with desktop publishing software like Aldus PageMaker including versions of Lorem Ipsum',
+            description:'Lorem ipsum dolor sit amet, consectetur adipiscing elit. Sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur. Excepteur sint occaecat cupidatat non proident, sunt in culpa qui officia deserunt mollit anim id est laborum.',
             price,
             geometry:{
                 type:"Point",
@@ -36,17 +36,13 @@ const seedDB = async () => {
             },
             images: [
                 {
-                    url: 'https://res.cloudinary.com/dmbr66tqw/image/upload/v1703408631/YelpCamp/ibg0cvhrw8lqzooszlm1.jpg',
+                    url: 'https://res.cloudinary.com/dmbr66tqw/image/upload/v1703408631/YelpCamp/iuuppvvacfr0heeufv9o.jpg',
                     filename: 'YelpCamp/ibg0cvhrw8lqzooszlm1',
                   },
                   {
                     url: 'https://res.cloudinary.com/dmbr66tqw/image/upload/v1703408631/YelpCamp/bcfboz251jdu0ctcdzn6.jpg',
                     filename: 'YelpCamp/bcfboz251jdu0ctcdzn6',
                   },
-                  {
-                    url: 'https://res.cloudinary.com/dmbr66tqw/image/upload/v1703408631/YelpCamp/iuuppvvacfr0heeufv9o.jpg',
-                    filename: 'YelpCamp/iuuppvvacfr0heeufv9o',
-                  }
               
             ]
         })
